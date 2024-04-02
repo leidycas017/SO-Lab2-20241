72dcd9063ddbaece6c833d57f09ef396590f24c7
Leidy Castaño <leidy.castanoc@udea.edu.co>
2024-04-02 16:10:38 -0500

    commit init

---
+++ wish.c
@@ -0,0 +1,7 @@
+/*
+    Programa: Ejecución de lote de comandos
+    Autores:  [Leidy Castaño Castaño], [Yuly Yesenia Alvear Romo], [Omar Alberto Torres]
+    Profesor: [Dany Alexandro Munera ]
+    Curso:    [Sistemas operativos y laboratorio]
+    Fecha:    [Abril 10 del 2024]
+*/
