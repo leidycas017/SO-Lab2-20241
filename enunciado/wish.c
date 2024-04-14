1f9da5b1d2ebd67b113ff5553de78c18241830fc
YulyAR1 <yuly.alvear@udea.edu.co>
2024-04-13 19:29:44 -0500

    Main y rutas

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -1,4 +1,4 @@
-/*
+/*  
  * Programa: Ejecución de lote de comandos
  * Autores:  [Leidy Castaño Castaño], [Yuly Yesenia Alvear Romo], [Omar Alberto Torres]
  * Profesor: [Dany Alexandro Munera ]
@@ -19,19 +19,19 @@
 #define MAXIMA_LONGITUD_PATH 512
 #define MAXIMA_LONGITUD_COMANDO 512
 #define MAXIMOS_ARGUMENTOS 512
-#define MAXIMA_LOGITUD_LINEA 512
-#define MAXIMO_BUFFER 1024
-#define EXEC_SUCCESS(status) ((status) == 0)
-#define MAX_INPUT_LENGTH 1024
+#define MAXIMA_LOGITUD_LINEA  512
+#define MAXIMO_BUFFER  1024
+#define EXEC_SUCCESS(status) ((status) == 0) 
+#define  MAX_INPUT_LENGTH 1024
 
 /**************************************** Variables globales ***************************************/
-char **rutas;
+char **rutas; 
 int nume_rutas = 0;
 int capacidad_rutas = 256;
 
 /*********************************** Definición prototipos de funciones *****************************/
-FILE *getFile(int argc, char *argv[]);
-char **almacenarArgumentos(char *token, char *line_copy);
+FILE* getFile(int argc, char *argv[]);
+char** almacenarArgumentos(char *token, char *line_copy) ;
 void error();
 void error_Three(char *msg);
 void prompt();
@@ -44,7 +44,7 @@ int procesoTres(int argc, char *argv[]);
 void parsear_comandos(char *comando, char **args, int *segundoplano);
 void ejecutar_comando(char **args, int segundoplano);
 void procesoTwo(char *comando);
-void proceso(char *comando);
+void  proceso(char * comando);
 int contiene_ampersand(const char *cadena);
 void redirigir_salida_entrada_a_archivos(char *comando);
 void agregar_ruta(const char *nueva_ruta);
@@ -53,23 +53,19 @@ void liberar_rutas();
 /***************************************** Función principal ***************************************/
 
 /** La funcion main controla el flujo del program y ciclo del proceso interactivo ******************/
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     inicializar_rutas();
     nume_rutas = sizeof(rutas) / sizeof(rutas[0]);
     char comando[MAXIMA_LONGITUD_COMANDO];
 
-    if (argc == 2)
-    {
+    if (argc == 2) {
         procesoTres(argc, argv); // Se ejecuta proceso batch si se proporciona un argumento
         return 0;
     }
 
-    while (1)
-    {
+    while (1) {
         prompt();
-        if (fgets(comando, MAXIMA_LONGITUD_COMANDO, stdin) == NULL)
-        {
+        if (fgets(comando, MAXIMA_LONGITUD_COMANDO, stdin) == NULL) {
             // Si fgets retorna NULL, indica que se llegó al final del archivo (EOF)
             break; // Salir del bucle
         }
@@ -77,26 +73,19 @@ int main(int argc, char *argv[])
         // Eliminar el carácter de nueva línea del final del comando
         comando[strcspn(comando, "\n")] = '\0';
 
-        if (strcmp(comando, "exit") == 0)
-        {
+        if (strcmp(comando, "exit") == 0) {
             break; // Salir del bucle si se ingresa "exit"
         }
 
-        if (strcmp(comando, "") == 0)
-        {
+        if (strcmp(comando, "") == 0) {
             continue; // Volver a solicitar el comando si se ingresó una línea vacía
         }
 
-        if (contiene_ampersand(comando))
-        {
+        if (contiene_ampersand(comando)) {
             proceso(comando); // Se ejecutan comandos en segundo plano
-        }
-        else if (strchr(comando, '>') || strchr(comando, '<'))
-        {
+        } else if (strchr(comando, '>') || strchr(comando, '<')) {
             redirigir_salida_entrada_a_archivos(comando); // Se maneja el redireccionamiento
-        }
-        else
-        {
+        } else {
             procesoOne(comando); // Se procesan los comandos internos/externos
         }
     }
@@ -105,31 +94,25 @@ int main(int argc, char *argv[])
 }
 
 /*********************Liberar memoria dinámica******************************/
-void error_Three(char *msg)
-{
-    write(STDERR_FILENO, msg, strlen(msg));
+void error_Three(char *msg){
+    write(STDERR_FILENO, msg, strlen(msg)); 
 }
-void liberar_rutas()
-{
-    for (int i = 0; i < nume_rutas; i++)
-    {
+void liberar_rutas() {
+    for (int i = 0; i < nume_rutas; i++) {
         free(rutas[i]);
     }
     free(rutas);
 }
 
 /****************Inicializa ruta para busqueda de comandos************/
-void agregar_ruta(const char *nueva_ruta)
-{
-    if (nume_rutas >= capacidad_rutas)
-    { // Verificar si hay espacio suficiente
+void agregar_ruta(const char *nueva_ruta) {
+    if (nume_rutas >= capacidad_rutas) { // Verificar si hay espacio suficiente
         // Duplicar la capacidad del arreglo
         capacidad_rutas *= 2;
-        rutas = realloc(rutas, capacidad_rutas * sizeof(char *));
-        if (rutas == NULL)
-        {
+        rutas = realloc(rutas, capacidad_rutas * sizeof(char*));
+        if (rutas == NULL) {
             error();
-            return;
+           return;
         }
     }
     // Agregar la nueva ruta
@@ -140,9 +123,8 @@ void agregar_ruta(const char *nueva_ruta)
  *   La funcion inicializar rutas crea dinámicamente un array predeterminando algunas rutas
  *   compatibles con Unix y sistema operativo MAC
  */
-void inicializar_rutas()
-{
-    rutas = malloc(MAXIMA_LONGITUD_PATH * sizeof(char *));
+void inicializar_rutas() {
+    rutas = malloc(MAXIMA_LONGITUD_PATH * sizeof(char*));
     rutas[0] = strdup("/bin/");
     rutas[1] = strdup("/usr/bin/");
     rutas[2] = strdup("/path/to/date/directory/");
@@ -152,7 +134,7 @@ void inicializar_rutas()
     rutas[6] = strdup("/etc/");
     rutas[7] = strdup("/home/");
     rutas[8] = strdup("/lib/");
-}
+ }
 
 /******************************** Procesos por lotes **********************************************/
 /**
@@ -164,44 +146,62 @@ void inicializar_rutas()
  *    el control a shell para que ejecute todos los comandos del script y ya no tenemos que iterar sobre el script
  *    para ejecutar los comandos. La magia la ordena el sistema operativo al shell.
  */
-
-int procesoTres(int argc, char *argv[])
-{
+ 
+int procesoTres(int argc, char *argv[]) {
     FILE *script = fopen(argv[1], "r");
-
-    if (argc != 2 || script == NULL)
-    {
-        error();
+    
+    if (argc != 2 || script == NULL) {
+        error(); 
         return 0; // Retorna 1 en caso de error
     }
 
     // Crear un proceso hijo
     pid_t pid = fork();
-    if (pid < 0)
-    {
+    if (pid < 0) {
         error();
         return 0; // Retorna 1 en caso de error
-    }
-    else if (pid == 0)
-    {
+    } else if (pid == 0) {
         // Proceso hijo
-        char *comando = strdup("/bin/sh");       // Obtener la ruta del shell
+        char *comando = strdup("/bin/sh"); // Obtener la ruta del shell
         char *args[] = {comando, argv[1], NULL}; // Argumentos para ejecutar el script
-        execvp(args[0], args);                   // Ejecutar el script de shell
-        error();                                 // Si execvp falla
+        execvp(args[0], args); // Ejecutar el script de shell
+        error(); // Si execvp falla
         return 1;
-    }
-    else
-    {
+    } else {
         // Proceso padre
         int status;
         waitpid(pid, &status, 0);
-        if (!EXEC_SUCCESS(status))
-        {
-            error();  // Mostrar mensaje de error si la ejecución falla
+        if (!EXEC_SUCCESS(status)) {
+            error(); // Mostrar mensaje de error si la ejecución falla
             return 0; // Retorna 1 en caso de error
         }
     }
 
     return 1; // Retorna 1 si todo se ejecutó correctamente
+}
+
+/**La función prompt() muestra el símbolo de shell */
+void prompt() {
+    fprintf(stdout, "%s", "wish> ");
+}
+
+/** Toma una línea de entrada(comando) y la divide en tokens que se almacenan en el arreglo de cadenas args */
+void parsear_comando(char *comando, char **args) {
+    char *token;                                       // Puntero token(argumento o comando)
+    int i = 0;
+    // Se ejecuta mientras existan tokens y no se haya alcanzado el máximo número de tokens
+    while ((token = strsep(&comando, " \n")) != NULL && i < MAXIMOS_ARGUMENTOS - 1) {
+        if (*token != '\0') {
+            args[i++] = token;                     // Se almacena token
+        }
+    }
+    args[i] = NULL;       // Marca el final de la lista de argumentos
+}
+
+/**
+ * Error genérico para todas las ocurrencias de error
+ */
+void error(){
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message)); 
 }
